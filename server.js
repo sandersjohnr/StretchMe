6eef53cd1de5a256ec30a6e0c7645556b704ced8
John Sanders <sandersjohnr@gmail.com>
2015-04-19 10:32:03 -0400

    finding git baseline

--- server.js
+++ server.js
@@ -1,34 +1,3 @@
-
-/*
-var application_root = __dirname,
-    express          = require('express'),
-    bodyParser       = require('body-parser'),
-    path             = require('path'),
-    logger           = require('morgan'),
-    campusRouter     = require('./routers/campus_router.js'),
-    courseRouter     = require('./routers/course_router.js'),
-    studentRouter    = require('./routers/student_router.js');
-
-var app = express();
-
-// Server Configuration
-if (process.env.NODE_ENV !== "test") {
-  app.use( logger('dev') );
-}
-app.use( bodyParser.urlencoded({ extended: false }) );
-app.use( bodyParser.json() );
-app.use( express.static( path.join( application_root, 'public' )));
-app.use( express.static( path.join( application_root, 'browser' )));
-
-app.use('/students', studentRouter);
-app.use('/courses', courseRouter);
-app.use('/campuses', campusRouter);
-
-
-// Export app as module
-module.exports = app;
-*/
-
 var express       = require('express'),
     session       = require('express-session'),
     morgan        = require('morgan'),
