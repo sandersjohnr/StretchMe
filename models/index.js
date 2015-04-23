0ad2d2813fa5c797500b886cf41ff1caf069b227
John Sanders <sandersjohnr@gmail.com>
2015-04-23 18:24:59 -0400

    stupid heroku

--- models/index.js
+++ models/index.js
@@ -6,7 +6,6 @@ var Sequelize = require("sequelize");
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 
 if (process.env.HEROKU_POSTGRESQL_AQUA_URL) {
